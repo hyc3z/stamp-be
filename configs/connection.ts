e690ebd284b6a2faf40a4cdbe3e565463a4487a6
hyc <tubao9hao@126.com>
2021-03-30 11:52:04 +0800

    feat: init service.

--- configs/connection.ts
+++ configs/connection.ts
@@ -1,11 +1,13 @@
 import * as entities from 'app/entities'
 import * as bootstrap from './bootstrap'
 import { print } from './utils'
-import { Connection, createConnection, getConnectionOptions } from 'typeorm'
+import { Connection, createConnection, getConnectionOptions, getConnection } from 'typeorm'
 import { exec } from 'child_process';
 import { MysqlConnectionCredentialsOptions } from 'typeorm/driver/mysql/MysqlConnectionCredentialsOptions';
+import { StampUser } from 'app/entities';
+import { UserService } from 'app/services';
 let connection : Connection;
-async function conn () {
+async function connDB () {
   const connectionOptions = await getConnectionOptions()
   connection = await createConnection({
     ...connectionOptions,
@@ -16,12 +18,19 @@ async function conn () {
   } else {
     print.danger('Database connection failed.')
   }
-
+  await initDatabase()
   bootstrap.connected()
 }
 
+async function startSlurm() {
+  const serviceList = ["slurmd", "slurmctld", "slurmdbd", "slurmrestd"]
+  serviceList.forEach(service => {
+    exec(`systemctl start ${service}`)
+  })
+}
 ;(async function () {
-  await conn()
+  await connDB()
+  await startSlurm()
 })().catch(async error => {
   console.log(error)
   if(error.errno === 1049) {
@@ -30,12 +39,21 @@ async function conn () {
     const { username, password, host, database } = connectionOptions as MysqlConnectionCredentialsOptions
     const createCommand = `mysql -u${username} -p${password} -h ${host} -e "CREATE SCHEMA ${database} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"`
     exec(createCommand)
-    await conn()
+    await connDB()
+  } else {
+    console.log("Fatal: Startup failed.")
   }
 })
-function initDatabase(connection: Connection) {
+async function initDatabase() {
   print.log('Checking database status...')
-  connection.query("select * from ")
+  const initAdminUsername = "admin"
+  const initAdminPwd = "123456"
+  const response =  await UserService.registerAdmin(initAdminUsername, initAdminPwd);
+  console.log("Register admin:", response)
+  const initUserUsername = "user"
+  const initUserPwd = "123456"
+  const response2 =  await UserService.registerUser(initUserUsername, initUserPwd);
+  console.log("Register user:", response2)
 }
 
 export default connection;
